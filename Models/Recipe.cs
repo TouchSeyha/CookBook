19d335020a7ba40dcb0d373bf2846430267fc26f
HaCurly <Seyha Touch>
2024-02-14 11:05:46 +0700

    D

--- Models/Recipe.cs
+++ Models/Recipe.cs
@@ -11,9 +11,10 @@ namespace CookBook.Models
     {
         public string Name { get; set; }
         public string Type { get; set; }
-        public string Cuisine { get; set; }
+        public string Cuisine { get; set; } = "";
         public string Ingredients { get; set; }
         public string Instructions { get; set; }
+        public List<string> Images { get; set; } = new List<string>();
     }
 
 }
